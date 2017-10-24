e2e8286ffd3a52bc36224d32963af31b76ab4c75
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-10-24 15:57:16 +0300

    refactor data model for mongo db

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,22 +2,24 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
 
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
 public class Ingredient {
 
+    @Id
     private String id;
+
     private String description;
+
     private BigDecimal amount;
 
+    @DBRef
     private UnitOfMeasure uom;
-    private Recipe recipe;
 
     public Ingredient() {
     }
@@ -26,13 +28,6 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-    }
-
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-        this.recipe = recipe;
     }
 
 }
