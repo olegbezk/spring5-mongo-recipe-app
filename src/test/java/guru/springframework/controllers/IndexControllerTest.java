d0dae915126aec556ae41cddeeaba03167954128
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-10-27 14:29:27 +0300

    change index controller to the reactive type controller

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -5,11 +5,15 @@ import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.ui.Model;
 import reactor.core.publisher.Flux;
 
@@ -21,37 +25,46 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @Ignore
+@RunWith(SpringRunner.class)
+@WebFluxTest
+@Import(IndexController.class)
 public class IndexControllerTest {
 
-    @Mock
-    RecipeService recipeService;
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Mock
-    Model model;
+    private RecipeService recipeService;
 
-    IndexController controller;
+    @Mock
+    private Model model;
+
+    @Autowired
+    private IndexController controller;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
-        controller = new IndexController(recipeService);
+        webTestClient = WebTestClient.bindToController(controller).build();
     }
 
     @Test
     public void testMockMVC() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        // MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
         when(recipeService.getRecipes()).thenReturn(Flux.empty());
 
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+        //        mockMvc.perform(get("/"))
+        //                .andExpect(status().isOk())
+        //                .andExpect(view().name("index"));
+
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody();
     }
 
     @Test
